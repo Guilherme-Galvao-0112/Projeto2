0de28247aefd9a1e30a86da7365974d527bff85f
Lucas dos Santos Antunes <lucas.antunes.santos81@gmail.com>
2023-05-24 20:40:53 -0400

    adição model cargo

--- src/main/java/Model/Funcionario.java
+++ src/main/java/Model/Funcionario.java
@@ -8,6 +8,37 @@ package Model;
  *
  * @author dell
  */
+import java.util.Date;
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+/**
+ *
+ * @author dell
+ */
+@Entity
+@Table(name = "Funcionario")
 public class Funcionario {
     
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Integer ID;
+    private String Nome;
+    private String Endereco;
+    private String Email;
+    private String Telefone;
+    @OneToOne(cascade = ({CascadeType.DETACH)})
+    @JoinColumn(name= Id)
+    private Integer CargoID;
+    @OneToMany(targetEntity = Usuarios.class)
+    private String login;
+    private Date DataDeNascimento;
 }
+
